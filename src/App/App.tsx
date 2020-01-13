4061f7d240fb6701f0016e2693c6188fe8f3204a
Daniele Tortora <tortora87@gmail.com>
2020-01-13 13:56:43 +0000

    project basic setup

---
+++ src/App/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const App: React.FC = () => {
+  console.log(process.env.REACT_APP_PRODUCT_API);
+  return null;
+};
+
+export default App;
---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const ProductList: React.FC<Props> = () => {
+  return <div></div>;
+};
+
+export default ProductList;
